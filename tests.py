1decf0606ce0d5c1eb72be23fe5f27b63088cf9d
RichterChr <richterchr@gmx.net>
2025-07-23 20:10:59 +0200

    added comment

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from functions.run_python import run_python_file
 
+# Tests für run_python
+
 print(run_python_file("calculator", "main.py"))
 
 print(run_python_file("calculator", "main.py", ["3 + 5"]))
